55c0b1cf59ec11c96405121a6597bac0c4db0f78
Alexander Ulmer <ulmer@student.tugraz.at>
2021-12-11 11:05:25 +0100

    refactor repository filenames

--- src/aoc2020/mod.rs
+++ src/aoc2020/mod.rs
@@ -1,9 +1,9 @@
-mod day1;
+mod day01;
 mod day18;
 
 pub fn run(id: usize, input: String) -> Option<(u64, u64)> {
     Some(match id {
-        1 => day1::main(input),
+        1 => day01::main(input),
         18 => day18::main(input),
         _ => return None,
     })
--- src/aoc2021/mod.rs
+++ src/aoc2021/mod.rs
@@ -1,26 +1,26 @@
-mod day1;
+mod day01;
+mod day02;
+mod day03;
+mod day04;
+mod day05;
+mod day06;
+mod day07;
+mod day08;
+mod day09;
 mod day10;
 mod day11;
-mod day2;
-mod day3;
-mod day4;
-mod day5;
-mod day6;
-mod day7;
-mod day8;
-mod day9;
 
 pub fn run(id: usize, input: String) -> Option<(u64, u64)> {
     Some(match id {
-        1 => day1::main(input),
-        2 => day2::main(input),
-        3 => day3::main(input),
-        4 => day4::main(input),
-        5 => day5::main(input),
-        6 => day6::main(input),
-        7 => day7::main(input),
-        8 => day8::main(input),
-        9 => day9::main(input),
+        1 => day01::main(input),
+        2 => day02::main(input),
+        3 => day03::main(input),
+        4 => day04::main(input),
+        5 => day05::main(input),
+        6 => day06::main(input),
+        7 => day07::main(input),
+        8 => day08::main(input),
+        9 => day09::main(input),
         10 => day10::main(input),
         11 => day11::main(input),
         _ => return None,
