40d9c435163e8e8dfb95c9c2cc82ab957f0528f0
Alexander Ulmer <alexander.ulmer@gurdinet.at>
2022-12-04 11:30:32 +0100

    improve solution day4

--- src/aoc2022/day04.rs
+++ src/aoc2022/day04.rs
@@ -1,17 +1,17 @@
-use std::ops::RangeInclusive;
+use std::{collections::HashSet, ops::RangeInclusive};
 
 pub fn main(input: String) -> (u64, u64) {
     let pairs_fully_contained = input
         .lines()
         .map(|line| into_ranges(line))
-        .map(|(r1, r2)| check_contains(r1, r2))
+        .map(|(r1, r2)| contains(r1, r2))
         .map(|b| if b { 1 } else { 0 })
         .sum();
 
     let pairs_overlapping = input
         .lines()
         .map(|line| into_ranges(line))
-        .map(|(r1, r2)| check_overlaps(r1, r2))
+        .map(|(r1, r2)| overlaps(r1, r2))
         .map(|b| if b { 1 } else { 0 })
         .sum();
 
@@ -30,33 +30,22 @@ fn str2range(s: &str) -> RangeInclusive<u64> {
     start..=end
 }
 
-fn check_contains(r1: RangeInclusive<u64>, r2: RangeInclusive<u64>) -> bool {
-    if r1.contains(r2.start()) && r1.contains(r2.end()) {
-        true
-    } else {
-        if r2.contains(r1.start()) && r2.contains(r1.end()) {
-            true
-        } else {
-            false
-        }
-    }
+fn contains(r1: RangeInclusive<u64>, r2: RangeInclusive<u64>) -> bool {
+    let s1: HashSet<u64> = HashSet::from_iter(r1);
+    let s2: HashSet<u64> = HashSet::from_iter(r2);
+    let intersect_len = s1.intersection(&s2).count();
+    s1.len() == intersect_len || s2.len() == intersect_len
 }
 
-fn check_overlaps(r1: RangeInclusive<u64>, r2: RangeInclusive<u64>) -> bool {
-    if r1.contains(r2.start()) || r1.contains(r2.end()) {
-        true
-    } else {
-        if r2.contains(r1.start()) || r2.contains(r1.end()) {
-            true
-        } else {
-            false
-        }
-    }
+fn overlaps(r1: RangeInclusive<u64>, r2: RangeInclusive<u64>) -> bool {
+    let s1: HashSet<u64> = HashSet::from_iter(r1);
+    let s2: HashSet<u64> = HashSet::from_iter(r2);
+    s1.intersection(&s2).count() > 0
 }
 
 #[test]
-fn contains() {
-    assert_eq!(check_contains(6..=6, 4..=6), true);
-    assert_eq!(check_contains(2..=8, 3..=7), true);
-    assert_eq!(check_contains(2..=3, 4..=5), false);
+fn contains_test() {
+    assert_eq!(contains(6..=6, 4..=6), true);
+    assert_eq!(contains(2..=8, 3..=7), true);
+    assert_eq!(contains(2..=3, 4..=5), false);
 }
